0e527e31d94c70b724a90b0b73654f8ed38801de
Sumit Rawat <102050350+decipher23@users.noreply.github.com>
2026-07-14 17:20:05 +0530

    Update newsclassify2.py

--- newsclassify2.py
+++ newsclassify2.py
@@ -5,7 +5,7 @@ import sqlite3
 
 llm=ChatOpenAI(model="gpt-4o-mini")
 def data1(News,Newsclassified,prob):
-    conn=sqlite3.connect(r"C:\Users\rawat\OneDrive\Desktop\6PM\project2\review_project.db")
+    conn=sqlite3.connect("review_project.db")
     cursor=conn.cursor()
     query="insert into newstable(News,Newsclassified,prob) values(?,?,?)"
     values=(News,Newsclassified,prob)
